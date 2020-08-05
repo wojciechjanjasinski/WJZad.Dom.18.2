2adb82f6c6ba0f1e38ed70b26900913234465e31
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-08-05 21:48:39 +0200

    All tests pass.

--- src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
@@ -1,6 +1,5 @@
 package pl.javastart.exercise.streams;
 
-import org.graalvm.compiler.core.common.PermanentBailoutException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +19,7 @@ public class FilteringMachineTwo {
 
     public List<User> convertPeopleToUsers(List<Person> people) {
         return people.stream()
-                .map((Person t) -> User.getName(t))
-                .map(User::getAge)
-                .map(User::getNickname)
+                .map(person -> new User(person.getName(), person.getAge(), person.getName()+"_"+person.getAge()))
                 .collect(Collectors.toList());
     }
 }
